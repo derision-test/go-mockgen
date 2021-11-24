48e061a894a4cac4981ae4f815f6573def16a8c4
Eric Fritz <eric@eric-fritz.com>
2021-11-24 10:08:23 -0600

    Add NewStrictMockX constructor (#16)

---
+++ internal/integration/strict_test.go
@@ -0,0 +1,29 @@
+package integration
+
+import (
+	"context"
+	"testing"
+
+	"github.com/derision-test/go-mockgen/internal/integration/testdata/mocks"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStrictConstructor(t *testing.T) {
+	// All invocations panic by default
+	mock := mocks.NewStrictMockRetrier()
+
+	assert.Panics(t, func() {
+		_ = mock.Retry(context.Background(), func() error {
+			return nil
+		})
+	})
+
+	// Should not panic if overwritten
+	mock.RetryFunc.SetDefaultReturn(nil)
+
+	assert.NotPanics(t, func() {
+		_ = mock.Retry(context.Background(), func() error {
+			return nil
+		})
+	})
+}
